e1db3a22372013c00e98408b7927d8921ccdf4c9
mdtrooper <tres.14159@gmail.com>
2017-06-12 02:59:27 +0200

    Limpiando un poco el código.

--- bibliolab.sh
+++ bibliolab.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
 # Usamos trap para no permitir cerrar con control+C
 trap '' 1 2 3 15 20
+
 # Desactivamos la tecla ESC
-echo -e "keymaps 0-127\nkeycode 1 =" > /tmp/key.map; loadkeys /tmp/key.map
-db=libros.csv # archivo con el listado de libros
-dialog --no-shadow --colors --no-lines --no-kill --infobox "    \ZbBienvenid@ amig@\ZB \n  --------------------\n  2017 IngoberLAB #301\n  --------------------\n  BiblioLAB 0.1 - fiwi\n  --------------------\n  _.-._.-._.-._.-._.-_" 10 30; sleep 5
+echo -e "keymaps 0-127\nkeycode 1 =" > /tmp/key.map
+loadkeys /tmp/key.map
+
+# archivo con el listado de libros
+db="libros.csv"
+
+dialog --no-shadow --colors --no-lines --no-kill --infobox "    \ZbBienvenid@ amig@\ZB \n  --------------------\n  2017 IngoberLAB #301\n  --------------------\n  BiblioLAB 0.1 - fiwi\n  --------------------\n  _.-._.-._.-._.-._.-_" 10 30
+sleep 5
+
 paso1=0
 
 while true
@@ -13,7 +21,7 @@ do
 	then
 		while read -r libro
 		do
-			id=$(echo $libro | cut -d ";" -f 1);
+			id=$(echo $libro | cut -d ";" -f 1)
 			titulo=$(echo $libro | cut -d ";" -f 2)
 			estado=$(echo $libro | cut -d ";" -f 3)
 			correo=$(echo $libro | cut -d ";" -f 4)
@@ -23,8 +31,8 @@ do
 			else
 				libros+=("$id" "\Zb$titulo ($correo)\ZB")
 			fi
-		done < "$db"
-
+		done < $db
+		
 		opcion=$(dialog --no-shadow --colors --no-lines --no-kill --no-cancel --menu "\ZbLIBROS DE BIBLIOLAB DISPONIBLES AHORA MISMO\ZB" 0 0 0 "${libros[@]}" --output-fd 1)
 		if [ ! -z "$opcion" ]
 		then
@@ -34,38 +42,39 @@ do
 		else
 			unset opcion
 		fi
-
 	fi
-
+	
 	seleccionado=$(cat $db | head -n $opcion | tail -n 1)
 	sid=$(echo $seleccionado | cut -d ";" -f 1)
 	slibro=$(echo $seleccionado | cut -d ";" -f 2)
 	status=$(echo $seleccionado | cut -d ";" -f 3)
 	email=$(echo $seleccionado | cut -d ";" -f 4)
-
-	if [ $status -eq 0 ]; then
+	
+	if [ $status -eq 0 ]
+	then
 		op=$(dialog --no-lines --no-shadow --colors --no-lines --menu "$slibro" 10 0 2 "1" "Coger" --output-fd 1)
 	else
 		op=$(dialog --no-lines --no-shadow --colors --no-lines --menu "\Zb$slibro\ZB lo tiene $email" 10 0 2 "2" "Devolver" --output-fd 1)
 	fi
-
-	if [ $op ]; then
+	
+	if [ $op ]
+	then
 		paso1=0
-		if [ $op -eq 2 ]; then
+		if [ $op -eq 2 ]
+		then
 			sustitucion="$sid;$slibro;0;disponible"
 			sed "s/$seleccionado/$sustitucion/g" $db > /tmp/tmp.csv
 			cp /tmp/tmp.csv $db
 		fi
-
-		if [ $op -eq 1 ]; then
+		
+		if [ $op -eq 1 ]
+		then
 			umail=$(dialog --no-shadow --colors --no-cancel --inputbox "Ha de escribir su \ZbEmail\ZB\nNo sea Troll. BiblioLAB se basa en la autogestión y la confianza: " 10 0 --output-fd 1)
 			sustitucion="$sid;$slibro;1;$umail"
 			sed "s/$seleccionado/$sustitucion/g" $db > /tmp/tmp.csv
 			cp /tmp/tmp.csv $db
 		fi
-
 	else
 		paso1=0
 	fi
-
 done
